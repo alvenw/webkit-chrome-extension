51437078da482f56d90aef3b4c419bd2449ea61f
Alven Wang <alvenw@berkeley.edu>
2015-07-22 22:30:16 -0700

    refix everything

--- content.js
+++ content.js
@@ -1,12 +1,11 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if(request.message === "clicked_browser_action" ) {
-    	if ($("html").css("-webkit-filter") === "invert(1)") {
-    		console.log('hi');
+    	if($("html").css("webkit-filter") === "invert(1)") {
     		$("html").css("-webkit-filter", "invert(0)");
-    	} else {
-      	$("html").css("-webkit-filter", "invert(1)");
-      }
+    	} else{
+	      $("html").css("-webkit-filter", "invert(1)");
+    	}
     }
   }
 );
