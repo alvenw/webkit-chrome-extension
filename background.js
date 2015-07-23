3a16e2d0cfe69ffc255050005952e41e506415ab
Alven Wang <alvenw@berkeley.edu>
2015-07-22 22:25:52 -0700

    tried to add popup buttons

--- background.js
+++ background.js
@@ -1,7 +1,22 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   // Send a message to the active tab
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     var activeTab = tabs[0];
+//     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+//   });
+// });
+
+
+function click(e) {
+	console.log('asw');
+  chrome.tabs.executeScript(null,
+      {code:"document.style.-webkit-filter=" + e.target.id + ""});
+  window.close();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var button = document.querySelectorAll('button');
+  for (var i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', click);
+  }
 });
--- content.js
+++ content.js
@@ -10,3 +10,15 @@ chrome.runtime.onMessage.addListener(
     }
   }
 );
+
+
+function invert() {
+	if(request.message === "clicked_browser_action" ) {
+    	if ($("html").css("-webkit-filter") === "invert(1)") {
+    		console.log('hi');
+    		$("html").css("-webkit-filter", "invert(0)");
+    	} else {
+      	$("html").css("-webkit-filter", "invert(1)");
+      }
+    }
+  };
